678d2cc1e6827faedbfc3f7fee364c23528236bf
Alex Hubkovich <alex.gubkovich@gmail.com>
2022-11-15 19:46:57 +0300

    organized program.cs

--- WeEatKholodets/Program.cs
+++ WeEatKholodets/Program.cs
@@ -1,45 +1,18 @@
-using System.Globalization;
-using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc.Razor;
-using Microsoft.EntityFrameworkCore;
 using WeEatKholodets.Data;
-using WeEatKholodets.Models;
+using WeEatKholodets.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter(); 
-
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDbConfig(builder.Configuration);
 
 builder.Services.AddTransient<IMealRepository, EFMealRepository>();
 
-builder.Services.AddAuthentication()
-    .AddGoogle(options =>
-    {
-        options.ClientId = builder.Configuration["Google:ClientId"];
-        options.ClientSecret = builder.Configuration["Google:ClientSecret"];
-    });
+builder.Services.AddIdentityAndAuthenticationConfig(builder.Configuration);
 
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession();
+builder.Services.AddSessionConfig();
 
-builder.Services.AddPortableObjectLocalization(options => options.ResourcesPath = "Localization");
-builder.Services.Configure<RequestLocalizationOptions>(options => {
-    var supportedCultures = new List<CultureInfo>
-    {
-        new CultureInfo("en-US"),
-        new CultureInfo("en"),
-        new CultureInfo("ru-ru"),
-        new CultureInfo("ru")
-    };
-
-    options.DefaultRequestCulture = new RequestCulture("en-US");
-    options.SupportedCultures = supportedCultures;
-    options.SupportedUICultures = supportedCultures;
-});
+builder.Services.AddLocalizationConfig();
 
 builder.Services.AddRazorPages()
     .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
@@ -47,7 +20,6 @@ builder.Services.AddRazorPages()
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();

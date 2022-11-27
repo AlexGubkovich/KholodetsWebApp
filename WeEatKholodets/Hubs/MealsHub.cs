f12619f685c2f0b98df9148e721a9a2a49c7aaa7
Alex Hubkovich <alex.gubkovich@gmail.com>
2022-11-27 18:24:16 +0300

    finished mealsHub

--- WeEatKholodets/Hubs/MealsHub.cs
+++ WeEatKholodets/Hubs/MealsHub.cs
@@ -5,6 +5,6 @@ namespace WeEatKholodets.Hubs;
 public class MealsHub : Hub
 {
     public async Task SendMeal(string user){
-        await Clients.All.SendAsync("ReceiveMeal", user);
+        await Clients.Others.SendAsync("ReceiveMeal", user);
     }
 }
